ef78a7b86aab731bf23d8f9b91aec33c6dc67ea6
Eric Nguyen <112188727+eric-nguyen13@users.noreply.github.com>
2023-10-10 08:27:09 -0400

    Time: 2 ms (88.69%), Space: 46.2 MB (27.79%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public void reorderList(ListNode head) {
+        
+        //Find middle of list using a slow and fast pointer approach
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) { // Accounts for odd and even lists
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        //Reverse the second half of the list using a tmp variable
+        ListNode second = slow.next;
+        slow.next = null; // Sever the first half of the list from the next half
+        ListNode prev = null;
+        while (second != null) {
+            ListNode tmp = second.next;
+            second.next = prev;
+            prev = second;
+            second = tmp;
+        }
+
+        //Re-assign the pointers to match the pattern
+        ListNode first = head;
+        second = prev;
+        while (second != null) {
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+            first.next = second;
+            second.next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+}
+
+// Tried on my own. Reviewed solution.
