8ff99bb406e5a7506e5b98a796bbfca8d1d073ca
Eric Nguyen <112188727+eric-nguyen13@users.noreply.github.com>
2023-10-19 22:50:18 -0400

    Time: 1 ms (41.58%), Space: 43.2 MB (23.26%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,35 +1,13 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        // Initialize Variables
         int[] dp = new int[cost.length];
-        
-        // Hardcode the last two prices
-        dp[dp.length - 1] = cost[dp.length - 1];
-        dp[dp.length - 2] = cost[dp.length - 2];
-        
-        // Traverse array
-        for(int i = cost.length - 3; i >= 0; i--){
-            int oneStep = dp[i + 1];
-            int twoStep = dp[i + 2];
-            
-            if(oneStep < twoStep){
-                dp[i] = cost[i] + dp[i + 1];
-                System.out.println("one");
-            }
-            else if(twoStep < oneStep){
-                dp[i] = cost[i] + dp[i + 2];
-                System.out.println("two");
-            }
-            else{ // They are equal... figure out between 1 or 2 steps... 2 steps?
-                dp[i] = cost[i] + dp[i + 2];
-            }
+
+        dp[0] = cost[0]; 
+        dp[1] = cost[1];
+        for(int i = 2; i < cost.length; i++){
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
         }
         
-        for(int i = 0; i < dp.length; i++){
-            System.out.println(dp[i]);
-        }
-        
-        // Return output
-        return Math.min(dp[0], dp[1]);
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
     }
 }
