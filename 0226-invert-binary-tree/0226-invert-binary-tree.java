09f4085b27505fa79fe5134b2da389c25e7c8be8
Eric Nguyen <112188727+eric-nguyen13@users.noreply.github.com>
2024-02-05 10:37:26 -0500

    Time: 0 ms (100.00%), Space: 40.9 MB (55.81%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,40 +15,16 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        // Base Case:
-        if (root == null) {
-            return null;
-        }
-        
-        // Initialize variables
-        TreeNode ret = new TreeNode(root.val);
-        
-        // Invert tree
-        recursiveTraversal(root, ret);
-        
-        // Return output
-        return ret;
-    
-    }
-    
-    public static TreeNode recursiveTraversal(TreeNode root, TreeNode ret){
-        // Base Case
         if(root == null){
             return null;
         }
         
-        // Recursive Behavior
-        if(root.right != null){
-            ret.left = new TreeNode(root.right.val);
-            recursiveTraversal(root.right, ret.left); 
-        }
-        if(root.left != null){
-            ret.right = new TreeNode(root.left.val);
-            recursiveTraversal(root.left, ret.right);  
-        }
+        TreeNode newNode = new TreeNode(root.val);
         
-        return null;
+        newNode.right = invertTree(root.left);
+        newNode.left = invertTree(root.right);
+        
+        return newNode;
     }
-    
-    
+
 }
